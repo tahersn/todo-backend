23a4ce65f52e1221c6b69db57916c1240211bf5a
tahersn <taher.snoussi@esprit.tn>
2023-02-25 13:23:47 +0100

    model update

--- routes/model/todo.js
+++ routes/model/todo.js
@@ -4,7 +4,6 @@ const { Schema, model } = require ("mongoose");
 const Todo = new Schema({
     text: {
         type: String,
-        required : true,
     },
     finished: {default: false, type: Boolean},
 },
